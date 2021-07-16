0385a4bdb6684ea91386289ff4fc491bd1486e26
hamedfarv <hamedfarvardin@gmail.com>
2021-07-16 18:09:02 +0430

    while loop concepts

---
+++ conditions.py
@@ -0,0 +1,13 @@
+
+is_hot = False
+is_cold = False
+
+if is_hot:
+    print("Hot Day !!")
+elif is_cold:
+    print ("Ohh cold day")
+else:
+    print ("seems we have nice day")
+
+#### logical operators
+    and  , or , not 
---
+++ while_For_loop.py
@@ -0,0 +1,24 @@
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+print('Done')
+
+i = 1
+while i <= 5:
+    print('*' * i)
+    i += 1
+print('Done')
+
+#### For loop
+
+x = "Hamed Fravardin"
+print(x[2])
+
+
+for item in 'Hamed Farvardin':
+    print (item)
+
+for item in range():
+    print(item)
+
